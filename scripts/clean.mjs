8d98d4f1f96c6b6617af205ab7195bb7f160ac9a
Darrell <zh0521@gmail.com>
2026-09-08 10:53:38 +0900

    feat: refactor project structure to support EntropystudioFfi, update configurations and clean up unused files

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -8,6 +8,8 @@ await Promise.all([
   rm(new URL("cpp", root), { recursive: true, force: true }),
   rm(new URL("ios/generated", root), { recursive: true, force: true }),
   rm(new URL("js/generated", root), { recursive: true, force: true }),
-  rm(new URL("js/NativeEntropystudio.ts", root), { force: true }),
+  rm(new URL("ios/EntropystudioFfi.h", root), { force: true }),
+  rm(new URL("ios/EntropystudioFfi.mm", root), { force: true }),
+  rm(new URL("js/NativeEntropystudioFfi.ts", root), { force: true }),
   rm(new URL("js/index.ts", root), { force: true }),
 ]);
